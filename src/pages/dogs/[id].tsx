39394454862980a4a4c0ac7083e7629ee7841a89
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-03-24 10:29:08 +0000

    fetch single dog data

--- src/pages/dogs/[id].tsx
+++ src/pages/dogs/[id].tsx
@@ -1,3 +1,31 @@
+import { useRouter } from 'next/router'
+import { useContext, useState, useEffect } from 'react'
+import { AuthContext } from '../_app'
+
 export default function SingleDog() {
-  return <div>this is a dog</div>
+  const router = useRouter()
+  const { id } = router.query
+  const [results, setResults] = useState<any>()
+  const [loading, setLoading] = useState(true)
+  const accessToken = useContext(AuthContext)
+  useEffect(() => {
+    if (accessToken === null) return
+    const fetchData = async () => {
+      const res = await fetch(`https://api.petfinder.com/v2/animals/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      const json = await res.json()
+      console.log(json)
+      if (json.animal) {
+        setLoading(false)
+        setResults(json)
+      }
+    }
+    if (router.isReady) fetchData()
+  }, [accessToken])
+
+  if (loading) return <h1>Loading...</h1>
+  if (!loading && results.animal) return <div>{results.animal.name}</div>
 }
